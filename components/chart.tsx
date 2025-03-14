1344c27704593891c740f674e490f918142eef22
Jens Lindegaard <jensvalde@gmail.com>
2025-03-14 21:44:17 +0100

    new favicon

--- components/chart.tsx
+++ components/chart.tsx
@@ -66,7 +66,7 @@ export default function Chart({
   return (
     <div className="p-4 border rounded-lg bg-white shadow w-full">
       <h2 className="text-lg font-semibold mb-2">
-        {instrumentName[selectedInstrument]} - Time Series Analysis
+        {instrumentName[selectedInstrument]} - Dataset
       </h2>
 
       <div className="mb-4 flex flex-col md:flex-row items-center gap-4">
